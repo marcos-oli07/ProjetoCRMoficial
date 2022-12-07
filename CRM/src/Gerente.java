62305a8ec036255309cf85b8b4d4946f1b7ec859
MARIA EDUARDA SOUSA SILVA <08759092130@ifto.local>
2022-12-07 15:50:33 -0300

    Atendente

--- CRM/src/Gerente.java
+++ CRM/src/Gerente.java
@@ -1,16 +1,21 @@
 public class Gerente extends Funcionario{
-    private int geren_do_setor;
+private String gerente_setor;
+private int gerente_lista_funcio;
 
-    public int getGeren_do_setor() {
-        return this.geren_do_setor;
-    }
-    public void setGeren_do_setor(int geren_do_setor) {
-        this.geren_do_setor = geren_do_setor;
+    public String getGerente_setor() {
+        return this.gerente_setor;
     }
 
-    public void visual_atri(){
-        System.out.printf("\n\nGerente:\nId: "+getPessoa_id()+"\nNome: "+getPessoa_nome()+
-        "\nSálario: "+getFuncio_salario()+"\nCarga horária: "+getFuncio_cargaH()+
-        "\nId loja: "+getGeren_do_setor());
+    public void setGerente_setor(String gerente_setor) {
+        this.gerente_setor = gerente_setor;
     }
+
+    public int getGerente_lista_funcio() {
+        return this.gerente_lista_funcio;
+    }
+
+    public void setGerente_lista_funcio(int gerente_lista_funcio) {
+        this.gerente_lista_funcio = gerente_lista_funcio;
+    }
+
 }
